12659cd25ecd029057020edf7570c987677ea91c
michellecan <16948138+michellecan@users.noreply.github.com>
2020-08-20 20:16:59 -0700

    added post and comment frontend and completed course

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -3,16 +3,15 @@ import { SET_ALERT, REMOVE_ALERT } from "../actions/types";
 const initialState = [];
 
 export default function (state = initialState, action) {
+  const { payload } = action;
 
-    const { type, payload } = action;
-
-    switch (action.type) {
-        //switch statements below
-        case SET_ALERT:
-            return [...state, payload];
-        case REMOVE_ALERT:
-            return state.filter(alert => alert.id !== payload);
-        default:
-            return state
-    }
+  switch (action.type) {
+    //switch statements below
+    case SET_ALERT:
+      return [...state, payload];
+    case REMOVE_ALERT:
+      return state.filter((alert) => alert.id !== payload);
+    default:
+      return state;
+  }
 }
